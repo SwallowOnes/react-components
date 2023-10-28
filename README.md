b51c5c24284e2d9dafa0e4af36ff07931a1c998a
Alexander Budko <jager.selion@gmail.com>
2023-10-29 00:14:30 +0500

    init project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-components
+# React Class Components
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
+export default App;
